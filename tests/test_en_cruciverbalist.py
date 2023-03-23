dbadfa1ef1acf8ef48f6618e68ab7664214073c0
Jakub Dakowski <jakubdakowski@gmail.com>
2023-03-23 17:56:49 +0100

    remove trailing whitespaces

--- tests/test_en_cruciverbalist.py
+++ tests/test_en_cruciverbalist.py
@@ -11,20 +11,20 @@ def crossword1():
 
 
 class TestDB:
-    
+
     def runner(self, sql):
         return DB_CURSOR.execute(sql).fetchall()
-    
+
     def test_select_words(self):
         assert len(self.runner(
             "select answer from clues limit 10"
         )) == 10
-        
+
     def test_regex(self):
         assert self.runner(
             "select 'c' regexp '[abc]'"
         ) == [(1,)]
-        
+
     def test_select_by_regex(self):
         no_regex = self.runner("select answer from clues where substr(answer,1,1)='A'")
         regex = self.runner(r"""
@@ -36,11 +36,11 @@ class TestDB:
                             on rowid=row_answer where found=1""")
         assert set(no_regex) == set(regex)
 
-def test_findword_1st_column(crossword1):
+def test_findword_1st_column(crossword1: CrosswordImprovable):
     assert CRUCIVERBALIST.find_word(crossword1.colrow(True, 1))[1:3] == "XT"
-    
-def test_findword_1st_row(crossword1):
+
+def test_findword_1st_row(crossword1: CrosswordImprovable):
     assert CRUCIVERBALIST.find_word(crossword1.colrow(False, 1))[:2] == "EX"
-    
-def test_findword_1st_weird_row(crossword1):
+
+def test_findword_1st_weird_row(crossword1: CrosswordImprovable):
     assert len(CRUCIVERBALIST.find_word(crossword1.colrow(False, 3)))<=7
