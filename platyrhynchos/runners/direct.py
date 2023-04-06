5b33214ba383ef88225783785fb6ecdd04566a1d
Jakub Dakowski <jakubdakowski@gmail.com>
2023-04-06 14:20:56 +0200

    migrate to functiontrace

--- platyrhynchos/runners/direct.py
+++ platyrhynchos/runners/direct.py
@@ -1,26 +1,24 @@
-from random import Random
-from typing import Any
+from contextlib import suppress
+
 from ..cruciverbalists import EnglishSimpleCruciverbalist
 from ..crossword import CrosswordImprovable
-import yappi
+
+with suppress(ModuleNotFoundError):
+    import functiontrace
+    functiontrace.trace()
 
 cruciverbalist = EnglishSimpleCruciverbalist()
+
 crossword = CrosswordImprovable.make(cruciverbalist.start_word(), 10, 10)
 
-yappi.set_clock_type("cpu")
-yappi.start()
 for _ in range(10):
     to_add = cruciverbalist.choose_and_fill(crossword)
     if to_add[0] is None:
         break
-    crossword.add(*to_add)
-yappi.stop()
+    crossword.add(*to_add) # type: ignore
 
 print(crossword)
 print()
 # TODO: Fix cached_property in order for this to work:
 # print("\n".join(crossword.words))
 print("\n".join(crossword.words_horizontal|crossword.words_vertical))
-
-yappi.get_func_stats().print_all()
-yappi.get_thread_stats().print_all()
