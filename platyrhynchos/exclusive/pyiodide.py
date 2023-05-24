74cbea28ceb4c2e74dbef91672808f4ba321ca4b
PogromcaPapai <PogromcaPapai@users.noreply.github.com>
2023-05-24 13:16:48 +0000

    Apply automatic changes

--- platyrhynchos/exclusive/pyiodide.py
+++ platyrhynchos/exclusive/pyiodide.py
@@ -1,9 +1,11 @@
 from bitarray import bitarray
 from supabase4js import getRandom, getRegex, getRegexWithAlphabit
 
+
 def download_db(url: str):
     """Doesn't do anything lmao"""
     pass
+
 
 get_random = getRandom
 get_regex = getRegex
