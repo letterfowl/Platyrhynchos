03e87411e35a9227005add37d113bc00a0349a96
Jakub Dakowski <jakubdakowski@gmail.com>
2023-05-26 19:32:01 +0200

    Migrate downloading to metaflow, debug

--- platyrhynchos/cruciverbalists/en_simple.py
+++ platyrhynchos/cruciverbalists/en_simple.py
@@ -4,31 +4,18 @@ from tqdm_loggable.auto import tqdm
 
 from ..commons.alphabit import Alphabit
 from ..commons.exceptions import DatabaseException
-from ..commons.logger import logger
 from ..crossword.colrow import ColRow
 from ..exclusive import download_db, get_random, get_regex, get_regex_w_alphabit
 from .base import Cruciverbalist
 
-URL = "https://cryptics.georgeho.org/data/clues.csv?_stream=on&_size=max"
-RUN_WITH_ALPHABIT = True
-
-
 class EnglishSimpleCruciverbalist(Cruciverbalist):
+    URL = "https://cryptics.georgeho.org/data/clues.csv?_stream=on&_size=max"
+    RUN_WITH_ALPHABIT = True
+    
     def __init__(self) -> None:
         """Prepares the database"""
-        download_db(URL)
+        download_db(self.URL)
         super().__init__()
-
-    def _sql_regex(self, **kwargs):
-        """
-        Queries the database for regex. Uses get_regex_w_alphabit or get_regex query.
-        Keyword arguments are parsed to the query for string interpolation.
-        """
-        if RUN_WITH_ALPHABIT:
-            found = get_regex_w_alphabit(**kwargs)
-        else:
-            found = get_regex(**kwargs)
-        return found if (found := [j[0] for j in found.fetchall()]) else None
 
     def eval_colrow(self, colrow: ColRow) -> int:
         """
@@ -42,12 +29,12 @@ class EnglishSimpleCruciverbalist(Cruciverbalist):
         Select compatible words using regex. It accepts a list of regular expressions and checks all one by one.
         """
         for i in [i.upper() for i in regexes]:
-            if RUN_WITH_ALPHABIT:
+            if self.RUN_WITH_ALPHABIT:
                 # Returns an alphabit query
                 alp = Alphabit(i).to_query()
-                if ret := self._sql_regex(regex=i, alphabit=alp):
+                if ret := get_regex_w_alphabit(regex=i, alphabit=alp):
                     return ret
-            elif ret := self._sql_regex(regex=i):
+            elif ret := get_regex(regex=i):
                 return ret
         return None
 
--- platyrhynchos/exclusive/cpython.py
+++ platyrhynchos/exclusive/cpython.py
@@ -7,69 +7,46 @@ from tqdm_loggable.auto import tqdm
 from ..commons.alphabit import Alphabit
 from ..commons.logger import logger
 from ..commons.utils import app_dir
+import os
+from os.path import isfile
 
 _db_path = app_dir("user_cache_dir", "en_simple.db")
 _connection = duckdb.connect(database=_db_path)
 
-
 def cursor_execute(sql, **kwargs):
     cursor = _connection.cursor()
-    return cursor.execute(sql, kwargs)
+    return cursor.execute(sql, kwargs) if kwargs else cursor.execute(sql)
 
+def convert_result_to_list(func):
+    def wrapper(*args, **kwargs):
+        return [i[0] for i in func(*args, **kwargs).fetchall()]
+    return wrapper
 
 def download_db(url: str):
-    logger.info("Downloading the database")
-    head = requests.head(url, timeout=60)
-    total_size = int(head.headers.get("content-length", -1)) if head.ok else -1
-    with tqdm.wrapattr(NamedTemporaryFile("wb", suffix=".csv"), "write", total=total_size) as temp_file:
-        temp_file: _TemporaryFileWrapper
-        with requests.get(url, stream=True, timeout=60) as csv_stream:
-            for chunk in csv_stream.iter_content(chunk_size=128):
-                temp_file.write(chunk)
-        temp_file.flush()
-        logger.info("Finished download, converting")
-        cursor_execute(f"CREATE TABLE clues AS SELECT * FROM '{temp_file.name}';")
+    if not isfile(_db_path):
+        log_mess = f"Database does not exist. Please run the GetGerghoWords pipeline with `--duckdb_path={_db_path}` to fix this."
+        logger.error(log_mess)
+        raise DatabaseError(log_mess)
+    try:
+        cursor_execute("SELECT answer, alphabit FROM clues LIMIT 1")
+    except duckdb.CatalogException:
+        log_mess = f"Database in {_db_path} exists, but does not have a valid clues table. Please run the GetGerghoWords pipeline with `--duckdb_path={_db_path}` to fix this."
+        logger.error(log_mess)
+        raise DatabaseError(log_mess)
+    else:
+        logger.info("Database found and checked")
+        return
 
-    logger.info("Finished downloading the database")
-
-    # Preprocess the database
-    cursor_execute(
-        """
-        DELETE FROM clues WHERE answer IS NULL;
-        ALTER TABLE clues ADD alphabit BIT
-    """
-    )
-    answers = cursor_execute("SELECT rowid, answer FROM clues").fetchall()
-    with NamedTemporaryFile("w", suffix=".csv") as alphabit_cache:
-        alphabit_cache.writelines(
-            tqdm(
-                (f"{rowid},{Alphabit(answer).to_db()}\n" for rowid, answer in answers),
-                total=len(answers),
-            )
-        )
-        cursor_execute(
-            """
-        UPDATE clues
-        SET alphabit = (
-            SELECT alphabit
-            FROM read_csv($file, columns={row:int, alphabit:bit}) as new
-            WHERE clues.rowid = new.row
-        );
-        """,
-            file=alphabit_cache.name,
-        )
-    logger.info("Finished preparing the database")
-
-
-def get_regex_w_alphabit(regex: str, alphabit: int):
+@convert_result_to_list
+def get_regex_w_alphabit(regex: str, alphabit: str):
     return cursor_execute(
         f"select answer from clues where bit_count({alphabit} | alphabit)=length(alphabit) and regexp_matches(answer, '{regex}')"
-    ).fetch_all()
+    )
 
-
+@convert_result_to_list
 def get_regex(regex: str):
-    return cursor_execute(f"select answer from clues where regexp_matches(answer, '{regex}')").fetch_all()
+    return cursor_execute(f"select answer from clues where regexp_matches(answer, '{regex}')")
 
-
+@convert_result_to_list
 def get_random():
-    return cursor_execute("select answer from clues limit 1").fetch_one()
+    return cursor_execute("select answer from clues limit 1")
