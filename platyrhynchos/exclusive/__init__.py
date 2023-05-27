0aba1e420dbd63cf48975d4e7b9ef0e69700b2b6
Jakub Dakowski <jakubdakowski@gmail.com>
2023-05-27 17:59:02 +0200

    update docs and fix typos

--- platyrhynchos/exclusive/__init__.py
+++ platyrhynchos/exclusive/__init__.py
@@ -3,8 +3,8 @@ from sys import platform
 from ..commons.logger import logger
 
 if platform == "emscripten":
-    logger.info("Using pyiodide")
-    from .pyiodide import *
+    logger.info("Using pyodide")
+    from .pyodide import *
 else:
     logger.info("Using cpython")
     from .cpython import *
