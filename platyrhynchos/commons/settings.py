d7acfa4e22521cdc24245cb44325b96f79e04df2
Jakub Dakowski <jdakowski@fandom.com>
2023-06-18 21:14:47 +0200

    test settings

--- platyrhynchos/commons/settings.py
+++ platyrhynchos/commons/settings.py
@@ -3,15 +3,22 @@ from tempfile import NamedTemporaryFile
 
 from dynaconf import Dynaconf
 
-if platform == "emscripten":
-    from _stuff import settings_text
-
-    settings = Dynaconf()
+def load_settings_from_string(settings_text: str):
+    """Load settings from a string. Useful for testing."""
+    temp_settings = Dynaconf()
     with NamedTemporaryFile("w", suffix=".toml") as f:
         f.write(settings_text)
         f.flush()
-        settings.load_file(f.name)  # type: ignore
-        settings = settings["DEFAULT"]
+        temp_settings.load_file(f.name)  # type: ignore
+    return temp_settings
+
+if platform == "emscripten":
+    # Running in the browser, so we can't load settings from a file.
+    
+    # pylint: disable=import-error
+    from _stuff import settings_text
+
+    settings = load_settings_from_string(settings_text)["DEFAULT"]
     assert hasattr(settings, "debug"), "settings.debug not found"
 else:
     settings = Dynaconf(settings_files=["settings.toml", ".secrets.toml"], environments=True)
